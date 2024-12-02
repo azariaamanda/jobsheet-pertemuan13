b675589000421697c33cb6339d79485cd89511bb
azaria amanda <azaryaamanda@gmail.com>
2024-12-02 07:16:11 +0700

    Percobaan 4 contoh no 3

--- PengunjungCafe02.java
+++ PengunjungCafe02.java
@@ -2,11 +2,25 @@ import java.util.Scanner;
 public class PengunjungCafe02 {
     public static void daftarPengunjung(String... namaPengunjung) {
         System.out.println("Daftar Nama Pengunjung: ");
-        for (int i=0; i < namaPengunjung.length; i++){
-            System.out.println("- " + namaPengunjung[i]);
+        // for (int i=0; i < namaPengunjung.length; i++){
+        //     System.out.println("- " + namaPengunjung[i]);
+        for (String pengunjung : namaPengunjung) {
+            System.out.println("- " + pengunjung);
         }
     }
     public static void main(String[] args) {
         daftarPengunjung("Ali", "Budi", "Citra");
     }  
 }
+
+//     public static void daftarPengunjung(String... namaPengunjung, int... umurPengunjung) {
+//         System.out.println("Daftar Nama dan Umur Pengunjung:");
+//         for (int i = 0; i < namaPengunjung.length; i++) {
+//             System.out.println("- " + namaPengunjung[i] + " (Umur: " + umurPengunjung[i] + ")");
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//         daftarPengunjung("Ali", "Budi", "Citra", 25, 30, 35);
+//     }
+// }
